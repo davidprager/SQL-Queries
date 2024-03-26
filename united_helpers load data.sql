1fadd1f77267e1de654e6b70a2cb139f9f94934e
davidprager <84202740+davidprager@users.noreply.github.com>
2024-03-26 18:54:26 -0400

    Add files via upload

--- united_helpers load data.sql
+++ united_helpers load data.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `assignment`
 --
 
+create database united_helpers
+
+use united_helpers
+
 DROP TABLE IF EXISTS `assignment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
